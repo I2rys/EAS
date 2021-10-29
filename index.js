a30b0f7da5ace6f7b8052e0203473d20f4b14389
Trac3D1y <i2rys@protonmail.com>
2021-10-30 07:31:48 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,34 @@
+//Dependencies
+const Express_Param = require("express-param")
+const Simple_Exec = require("simple-exec")
+const Express = require("express")
+
+//Variables
+const Port = process.env.PORT || 8080
+const Web = Express()
+
+///Configurations
+//Express
+Web.use(Express_Param())
+
+//Main
+Web.get("/exec", function(req, res){
+    var command = req.fetchParameter(["cmd"]).cmd
+    command = decodeURI(command)
+
+    Main()
+    async function Main(){
+        const results = await Simple_Exec.executeSync(command)
+
+        console.log(results)
+        if(results.err){
+            res.send("Invalid command or something went wrong.")
+        }else{
+            res.json(results.output.split("\n"))
+        }
+    }
+})
+
+Web.listen(Port, ()=>{
+    console.log(`Server is running in ${Port}`)
+})
